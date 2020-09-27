07e999d3d10e364937818b989a89ed1c31ff4df9
Szymon Szewc <bilet.mejl@onet.pl>
2020-09-27 18:46:04 +0200

    27.09.2020''

--- Core/Src/dht.c
+++ Core/Src/dht.c
@@ -12,10 +12,10 @@
 void set_pin_input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
 		GPIO_InitTypeDef GPIO_InitStruct = {0};
-        GPIO_InitStruct.Pin = GPIO_Pin;
-        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(GPIOx,&GPIO_InitStruct);
+		GPIO_InitStruct.Pin = GPIO_Pin;
+		GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		HAL_GPIO_Init(GPIOx,&GPIO_InitStruct);
 }
 
 
